96d94f8b23df13c02662d5b61510ebd9bd03a92b
JoeEverest <everestjoseph11@yahoo.com>
2025-06-16 10:48:07 -0500

    chore: add ignore rule for dist directory in ESLint configuration

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -7,5 +7,6 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  { ignores: ["dist/**"] },
   tseslint.configs.recommended,
 ]);
